b4d03f62f2175f8037ffbaf59d56e9c4b1b586b8
Sahil Barua <sahilrik@gmail.com>
2022-01-29 12:58:41 +0530

    power of two

---
+++ week2/bitmanupulation/power-of-2.cpp
@@ -0,0 +1,32 @@
+// geeksforgeeks
+
+// Given a non-negative integer N. The task is to check if N is a power of 2. More formally, check if N can be expressed as 2x for some x.
+
+// Input: N = 1
+// Output: YES
+// Explanation:1 is equal to 2 
+// raised to 0 (20 = 1).
+
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isPowerofTwo(int n){
+    if(n==0){
+        return false;
+    }else if ((n&(n-1))==0)
+    {
+       return true; 
+    }else{
+        return false;
+    }
+    
+}
+
+int main(){
+    int N;
+    cout<<"enter the integer to be checked"<<endl;
+    cin>>N;
+
+    bool result = isPowerofTwo(N);
+    cout<<"result : "<<result<<endl;
+}
